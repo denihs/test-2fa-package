cf8a61cd58261af5eb260f3ad336a91db3d59aac
denihs <denilsonh2014@gmail.com>
2022-01-25 16:21:35 -0400

    Updating app with the new changes from the package

--- imports/ui/App.jsx
+++ imports/ui/App.jsx
@@ -8,9 +8,12 @@ export const App = () => {
   const [user, setUser] = useState(null);
   const [qrCode, setQrCode] = useState(null);
   const [code, setCode] = useState(null);
+  const [token, setToken] = useState(null);
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [shouldAskCode, setShouldAskCode] = useState(false);
+  const [shouldAskCodeWithoutPass, setShouldAskCodeWithoutPass] = useState(false);
+  const [shouldAskCodeWithoutPassAndToken, setShouldAskCodeWithoutPassAndToken] = useState(false);
   const [message, setMessage] = useState("");
 
   useEffect(() => {
@@ -46,7 +49,7 @@ export const App = () => {
           </button>
           <button
             onClick={() => {
-              Accounts.generate2faActivationQrCode((err, svg) => {
+              Accounts.generate2faActivationQrCode("Testing app name", (err, svg) => {
                 if (err) {
                   console.error("Error trying to log in", err);
                   return;
@@ -89,6 +92,31 @@ export const App = () => {
       }
       }>login?</button>
 
+      <button onClick={() => {
+        Accounts.has2faEnabled(username, (err, isEnabled) => {
+          if (err) {
+            console.error("Error verifying if user has 2fa enabled", err);
+            return;
+          }
+
+          console.log('has2faEnabled', {username, isEnabled});
+
+          if (isEnabled) {
+            Accounts.requestLoginTokenForUser({selector: "denilsonh2014@gmail.com"}, (err) => {
+              if (err) {
+                console.error("Error verifying if user has 2fa enabled", err);
+                return;
+              }
+              setShouldAskCodeWithoutPassAndToken(true);
+            });
+            return;
+          }
+          setShouldAskCodeWithoutPass(true);
+        });
+
+      }
+      }>LOGIN WITHOUT PASSWORD?</button>
+
           {shouldAskCode && <div>
             <input onChange={({target: {value}}) => setCode(value)}/>
             <button onClick={() => {
@@ -98,6 +126,30 @@ export const App = () => {
                 }
               })}
             }>validate</button>
+          </div>}
+          {(shouldAskCodeWithoutPass || shouldAskCodeWithoutPassAndToken) && <div>
+            <input onChange={({target: {value}}) => setToken(value)}/>
+            {shouldAskCodeWithoutPassAndToken ?
+              <div>
+                <input onChange={({target: {value}}) => setCode(value)}/>
+
+                <button onClick={() => {
+                  Meteor.passwordlessLoginWithTokenAnd2faCode("denilsonh2014@gmail.com", token, code,error => {
+                    if (error) {
+                      console.error("Error trying to log in (passwordlessLoginWithToken)", error);
+                    }
+                  })}
+                }>validate</button>
+              </div> :
+              <button onClick={() => {
+                Meteor.passwordlessLoginWithToken("denilsonh2014@gmail.com", token, error => {
+                  if (error) {
+                    console.error("Error trying to log in (passwordlessLoginWithToken)", error);
+                  }
+                })}
+              }>validate</button>
+              }
+
           </div>}
         <button onClick={() => {
           Accounts.createUser({
